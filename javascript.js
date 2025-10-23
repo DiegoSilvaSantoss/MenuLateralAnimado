b3dcbd74143c88ff1c9238a972824bf69feabe0f
Dhii Santos <diegoalphapro@gmail.com>
2025-10-23 20:16:18 -0300

    Reajustes em Geral

--- javascript.js
+++ javascript.js
@@ -1,26 +1,21 @@
-/* 01 MAPEAMENTO DAS CLASSES DO HTML*/
-const botao = document.querySelector('.botao-menu')
- const menuLateral = document.querySelector('.menu-lateral')
- const conteudo = document.querySelector('.conteudo')
- const background = document.querySelector('.background')
+const botaoMenu = document.querySelector('.botao-menu')
+const menuLateral = document.querySelector('.menu-lateral')
+const conteudo = document.querySelector('.conteudo')
+const fundo = document.querySelector('.fundo')
 
- /* 02 FICAR DE OLHO NO BOTÃO AO CLICAR */
- botao.addEventListener('click', () => {
-
-    /* 03 ADICIONAR A CLASSE ATIVO NAS CLASSES NO HTML */
+botaoMenu.addEventListener('click', () => {
+    botaoMenu.classList.toggle('ativo')
     menuLateral.classList.toggle('ativo')
-    botao.classList.toggle('ativo')
     conteudo.classList.toggle('ativo')
-    background.classList.toggle('ativo')                    /* IF E ELSE TERNÁRIO */
-    document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
- })
+    fundo.classList.toggle('ativo')
+    document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#fff'
+})
 
 
-/* 04 FICAR DE OLHO NO BACKGROUND AO CLICAR, REMOVER A CLASSE ATIVO DAS CLASSES E MUDAR A COR DO BACKGROUND */
- background.addEventListener('click', () => {
+fundo.addEventListener('click', () => {
+    botaoMenu.classList.remove('ativo')
     menuLateral.classList.remove('ativo')
-    botao.classList.remove('ativo')
     conteudo.classList.remove('ativo')
-    background.classList.remove('ativo')
-    document.body.style.backgroundColor = '#ecf0f1'
- })
+    fundo.classList.remove('ativo')
+    document.body.style.backgroundColor = '#fff'
+})
